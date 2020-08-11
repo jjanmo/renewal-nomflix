d3d8794a64bda15432eaafc6a44b7c592490f685
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-12 02:38:52 +0900

    Updates

---
+++ precache-manifest.9a0a4637d8fb4150fd15f85d247b2cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5ad9009b6625e68867050b757b6ccc",
+    "url": "/renewal-nomflix/index.html"
+  },
+  {
+    "revision": "c97870c9d33fddd94277",
+    "url": "/renewal-nomflix/static/js/2.f214021f.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/renewal-nomflix/static/js/2.f214021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45aec434c346f6eb1180",
+    "url": "/renewal-nomflix/static/js/main.fb5f747c.chunk.js"
+  },
+  {
+    "revision": "379498ec121272c1b34c",
+    "url": "/renewal-nomflix/static/js/runtime-main.aab67097.js"
+  },
+  {
+    "revision": "078123121201147b7d88c1848371f6eb",
+    "url": "/renewal-nomflix/static/media/loader.07812312.gif"
+  },
+  {
+    "revision": "9ebb05f3d015c970dfe446e5f575f3c6",
+    "url": "/renewal-nomflix/static/media/logo_blue.9ebb05f3.png"
+  }
+]);
