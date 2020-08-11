5065db3b1cd63eb0fc3ab64267379921711dd72c
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-12 03:06:50 +0900

    Update loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,18 +2,33 @@ import React from 'react';
 import loader from 'assets/loader.gif';
 import styled from 'styled-components';
 
-const Container = styled.div`
-	text-align: center;
+const Container = styled.div``;
+
+const Image = styled.div`
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translateX(-50%);
+    background-image: url(${loader});
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    @media screen and (max-width: 1280px) {
+        width: 200px;
+        height: 200px;
+    }
+    @media screen and (min-width: 1281px) {
+        width: 300px;
+        height: 300px;
+    }
 `;
 
-const Image = styled.img``;
-
 function Loader() {
-	return (
-		<Container>
-			<Image src={loader} />
-		</Container>
-	);
+    return (
+        <Container>
+            <Image />
+        </Container>
+    );
 }
 
 export default Loader;
