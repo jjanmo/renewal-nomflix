a41f137844598d8d7a913ecfbe9be4f3480c8d1b
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-08-17 00:13:17 +0900

    Update css style

--- src/Components/Actors.js
+++ src/Components/Actors.js
@@ -5,148 +5,143 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Container = styled.div`
-	font-family: Verdana, Geneva, Tahoma, sans-serif;
-	font-size: 1.1rem;
-	margin: 1rem 0;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1.1rem;
+    margin: 1rem 0;
 `;
 const Title = styled.h2`
-	text-transform: uppercase;
-	font-weight: 550;
-	font-size: 1.2rem;
-	color: #eee;
-	margin: 0.5rem 0 1rem;
+    text-transform: uppercase;
+    font-weight: 550;
+    font-size: 1.2rem;
+    color: #eee;
+    margin: 0.5rem 0 1rem;
 `;
 const List = styled.ul`
-	display: grid;
-	grid-template-columns: repeat(8, 100px);
-	grid-gap: 0.5rem;
-	justify-content: left;
-
-	@media (max-width: 1440px) {
-		grid-template-columns: repeat(8, 80px);
-		grid-template-rows: ${(props) =>
-			props.isOver ? 'repeat(2, 95px)' : 'repeat(1, 95px)'};
-	}
-	@media (min-width: 1441px) {
-		grid-template-columns: repeat(8, 120px);
-		grid-template-rows: ${(props) =>
-			props.isOver ? 'repeat(2, 140px)' : 'repeat(1, 140px)'};
-	}
+    display: grid;
+    grid-template-columns: repeat(8, 100px);
+    grid-gap: 0.5rem;
+    justify-content: left;
+    @media (max-width: 1440px) {
+        grid-template-columns: repeat(8, 80px);
+        grid-template-rows: ${(props) => (props.isOver ? 'repeat(2, 95px)' : 'repeat(1, 95px)')};
+    }
+    @media (min-width: 1441px) {
+        grid-template-columns: repeat(8, 130px);
+        grid-template-rows: ${(props) => (props.isOver ? 'repeat(2, 140px)' : 'repeat(1, 140px)')};
+    }
 `;
-
 const Item = styled.li`
-	list-style: none;
-	border-radius: 5px;
-	width: 100%;
-	height: 100%;
-	position: relative;
-	overflow: hidden;
-	cursor: pointer;
-	transition: all 0.3s ease-in-out;
-	&:hover {
-		transform: scale(1.4);
-		z-index: 1;
-		box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-	}
+    list-style: none;
+    border-radius: 5px;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+        transform: scale(1.4);
+        z-index: 1;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    }
 `;
 const Profile = styled.div`
-	background-image: url(${(props) => props.profileUrl});
-	background-position: center center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	width: 100%;
-	height: 100%;
+    background-image: url(${(props) => props.profileUrl});
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
 `;
 const Name = styled.div`
-	font-size: 1rem;
-	font-weight: 600;
-	position: absolute;
-	left: 50%;
-	bottom: 5px;
-	transform: translate(-50%, 0);
-	text-align: center;
-	transition: opacity 0.2s ease-in-out;
-	opacity: 1;
-	${Item}:hover & {
-		opacity: 0;
-	}
+    font-size: 1rem;
+    font-weight: 600;
+    position: absolute;
+    left: 50%;
+    bottom: 5px;
+    transform: translate(-50%, 0);
+    text-align: center;
+    transition: opacity 0.2s ease-in-out;
+    opacity: 1;
+    ${Item}:hover & {
+        opacity: 0;
+    }
 `;
 const Overlay = styled.div`
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	transition: opacity 0.2s ease-in-out;
-	background-color: rgba(0, 0, 0, 0.5);
-	${Item}:hover & {
-		background-color: ${(props) =>
-			props.isProfile ? 'transparent' : 'rgba(0, 0, 0, 0.5)'};
-	}
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 0.2s ease-in-out;
+    background-color: rgba(0, 0, 0, 0.5);
+    ${Item}:hover & {
+        background-color: ${(props) => (props.isProfile ? 'transparent' : 'rgba(0, 0, 0, 0.5)')};
+    }
 `;
 
 function Actors({ id, isMovie = false }) {
-	const [actors, setActors] = useState([]);
-	const [isOver, setIsOver] = useState(false);
+    const [actors, setActors] = useState([]);
+    const [isOver, setIsOver] = useState(false);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				if (isMovie) {
-					const {
-						data: { cast },
-					} = await movieApi.getCast(id);
-					const _cast = cast.slice(0, 16);
-					setActors(_cast);
-					if (_cast.length > 8) setIsOver(true);
-				} else {
-					const {
-						data: { cast },
-					} = await tvApi.getCast(id);
-					const _cast = cast.slice(0, 16);
-					setActors(_cast);
-					if (_cast.length > 8) setIsOver(true);
-				}
-			} catch (e) {
-				console.log(e);
-			}
-		};
-		fetchData();
-	}, [id, isMovie]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (isMovie) {
+                    const {
+                        data: { cast },
+                    } = await movieApi.getCast(id);
+                    const _cast = cast.slice(0, 16);
+                    setActors(_cast);
+                    if (_cast.length > 8) setIsOver(true);
+                } else {
+                    const {
+                        data: { cast },
+                    } = await tvApi.getCast(id);
+                    const _cast = cast.slice(0, 16);
+                    setActors(_cast);
+                    if (_cast.length > 8) setIsOver(true);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchData();
+    }, [id, isMovie]);
 
-	return (
-		actors &&
-		actors.length > 0 && (
-			<Container>
-				<Title>actors</Title>
-				<List isOver={isOver}>
-					{actors.map((actor, index) => (
-						<Link to={`/actor/${actor.id}`} key={index}>
-							<Item>
-								<Overlay isProfile={!!actor.profile_path} />
-								<Profile
-									profileUrl={
-										actor.profile_path
-											? `https://image.tmdb.org/t/p/w200${actor.profile_path}`
-											: require('../assets/no_poster.png')
-									}
-								/>
-								<Name>{actor.name}</Name>
-							</Item>
-						</Link>
-					))}
-				</List>
-			</Container>
-		)
-	);
+    return (
+        actors &&
+        actors.length > 0 && (
+            <Container>
+                <Title>actors</Title>
+                <List isOver={isOver}>
+                    {actors.map((actor, index) => (
+                        <Link to={`/actor/${actor.id}`} key={index}>
+                            <Item>
+                                <Overlay isProfile={!!actor.profile_path} />
+                                <Profile
+                                    profileUrl={
+                                        actor.profile_path
+                                            ? `https://image.tmdb.org/t/p/w200${actor.profile_path}`
+                                            : require('../assets/no_poster.png')
+                                    }
+                                />
+                                <Name>{actor.name}</Name>
+                            </Item>
+                        </Link>
+                    ))}
+                </List>
+            </Container>
+        )
+    );
 }
 
 Actors.propTypes = {
-	isOver: PropTypes.bool,
-	actor: PropTypes.arrayOf({
-		id: PropTypes.string.isRequired,
-		profile_path: PropTypes.string,
-	}),
+    isOver: PropTypes.bool,
+    actor: PropTypes.arrayOf({
+        id: PropTypes.string.isRequired,
+        profile_path: PropTypes.string,
+    }),
 };
 
 export default Actors;
--- src/Components/MovieContent.js
+++ src/Components/MovieContent.js
@@ -9,225 +9,216 @@ import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Container = styled.div`
-	display: grid;
-	grid-template-columns: 65% 35%;
+    display: grid;
+    grid-template-columns: 65% 35%;
 `;
 const LeftBox = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: left;
-	padding-left: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    padding-left: 5rem;
 `;
 const Title = styled.h1`
-	font-size: 3.5rem;
-	margin: 1rem 0 0.5rem;
-	display: flex;
-	justify-content: left;
-	align-items: center;
+    font-size: 3.5rem;
+    margin: 1rem 0 0.5rem;
+    display: flex;
+    justify-content: left;
+    align-items: center;
 `;
 const SubTitle = styled.span`
-	font-size: 2rem;
-	color: #eee;
-	font-style: italic;
-	margin-bottom: 1rem;
+    font-size: 2rem;
+    color: #eee;
+    font-style: italic;
+    margin-bottom: 1rem;
 `;
 const Links = styled.div`
-	width: 100%;
-	display: flex;
-	justify-content: left;
-	align-items: center;
+    width: 100%;
+    display: flex;
+    justify-content: left;
+    align-items: center;
 `;
 const CollectionLink = styled(Link)`
-	font-family: Verdana, Geneva, Tahoma, sans-serif;
-	font-size: 1.1rem;
-	color: rgb(103, 193, 245);
-	background-color: #0e151d;
-	padding: 0.5rem 1rem;
-	border-radius: 5px;
-	margin-bottom: 1rem;
-	&:hover {
-		background-color: #60b4e4;
-		color: white;
-	}
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1.1rem;
+    color: rgb(103, 193, 245);
+    background-color: #0e151d;
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+    margin-bottom: 1rem;
+    &:hover {
+        background-color: #60b4e4;
+        color: white;
+    }
 `;
 const Classification = styled.div`
-	display: flex;
-	justify-content: left;
-	align-items: center;
-	margin-bottom: 1rem;
-	font-size: ${(props) => {
-		switch (props.genresLength) {
-			case 4: {
-				return '1rem';
-			}
-			case 5: {
-				return '0.9rem';
-			}
-			default: {
-				return '1.1rem';
-			}
-		}
-	}};
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    margin-bottom: 1rem;
+    font-size: ${(props) => {
+        switch (props.genresLength) {
+            case 4: {
+                return '1rem';
+            }
+            case 5: {
+                return '0.9rem';
+            }
+            default: {
+                return '1.1rem';
+            }
+        }
+    }};
 `;
 const Year = styled.span``;
 const Adult = styled.span`
-	color: '#c0392b';
+    color: '#c0392b';
 `;
 const Runtime = styled.span``;
 const Divider = styled.span`
-	margin: 0 10px;
+    margin: 0 10px;
 `;
 const IMDbLink = styled.a`
-	line-height: 0;
+    line-height: 0;
 `;
 const IMDbLogo = styled.img`
-	width: 50px;
+    width: 50px;
 `;
 const Overview = styled.div`
-	font-size: 1.2rem;
-	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 1.2rem;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 `;
 const RightBox = styled.div`
-	height: calc(100vh - 100px);
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+    height: calc(100vh - 100px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 const Poster = styled.div`
-	border-radius: 5px;
-	background-image: url(${(props) => props.posterUrl});
-	background-position: center center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-	opacity: ${(props) => (props.isExisted ? 1 : 0.5)};
-	@media (min-width: 1441px) {
-		width: 460px;
-		height: 680px;
-	}
-	@media (max-width: 1440px) {
-		width: 300px;
-		height: 400px;
-	}
+    border-radius: 5px;
+    background-image: url(${(props) => props.posterUrl});
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    opacity: ${(props) => (props.isExisted ? 1 : 0.5)};
+    @media (min-width: 1441px) {
+        width: 500px;
+        height: 700px;
+    }
+    @media (max-width: 1440px) {
+        width: 300px;
+        height: 400px;
+    }
 `;
 const Button = styled.button`
-	all: unset;
-	font-family: Verdana, Geneva, Tahoma, sans-serif;
-	font-size: 1rem;
-	color: rgb(103, 193, 245);
-	background-color: #0e151d;
-	padding: 0.5rem 1rem;
-	border-radius: 50%;
-	margin-left: 3%;
-	text-transform: uppercase;
-	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-	cursor: pointer;
-	&:hover {
-		background-color: #60b4e4;
-		color: white;
-	}
+    all: unset;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1rem;
+    color: rgb(103, 193, 245);
+    background-color: #0e151d;
+    padding: 0.5rem 1rem;
+    border-radius: 50%;
+    margin-left: 3%;
+    text-transform: uppercase;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    cursor: pointer;
+    &:hover {
+        background-color: #60b4e4;
+        color: white;
+    }
 `;
 
 function MovieContent({ history, movie }) {
-	const handleClick = () => {
-		history.goBack();
-	};
+    const handleClick = () => {
+        history.goBack();
+    };
 
-	return (
-		<>
-			<Container>
-				<LeftBox>
-					<Title>
-						{movie.title} <Button onClick={handleClick}>back</Button>
-					</Title>
-					<SubTitle>
-						{movie.original_title === movie.title
-							? ''
-							: `( ${movie.original_title} )`}
-					</SubTitle>
-					<Links>
-						{movie.belongs_to_collection && (
-							<CollectionLink
-								to={`/collection/${movie.belongs_to_collection.id}`}>
-								{movie.belongs_to_collection.name}
-							</CollectionLink>
-						)}
-					</Links>
-					<Classification genresLength={movie.genres && movie.genres.length}>
-						<Year>
-							{movie.release_date
-								? movie.release_date.slice(0, 4)
-								: 'Not Updated'}
-							<Divider>|</Divider>
-						</Year>
-						{movie.adult && (
-							<Adult>
-								Adult
-								<Divider>|</Divider>
-							</Adult>
-						)}
-						<Runtime>
-							{movie.runtime} min
-							<Divider>|</Divider>
-						</Runtime>
-						<Genres genres={movie.genres}></Genres>
-						<Rank score={movie.vote_average} totalVotes={movie.vote_count} />
-						<Divider>|</Divider>
-						{movie.imdb_id && (
-							<IMDbLink
-								href={`https://www.imdb.com/title/${movie.imdb_id}`}
-								target="_blank">
-								<IMDbLogo src="https://ia.media-imdb.com/images/M/MV5BMTk3ODA4Mjc0NF5BMl5BcG5nXkFtZTgwNDc1MzQ2OTE@._V1_.png" />
-							</IMDbLink>
-						)}
-					</Classification>
-					<Overview>{movie.overview}</Overview>
-					<Actors id={movie.id} isMovie={true} />
-					<Videos videos={movie.videos.results} />
-				</LeftBox>
-				<RightBox>
-					<Poster
-						posterUrl={
-							movie.poster_path
-								? `https://image.tmdb.org/t/p/w400${movie.poster_path}`
-								: require('../assets/no_poster.png')
-						}
-						isExisted={movie.poster_path && true}
-					/>
-				</RightBox>
-			</Container>
-		</>
-	);
+    return (
+        <>
+            <Container>
+                <LeftBox>
+                    <Title>
+                        {movie.title} <Button onClick={handleClick}>back</Button>
+                    </Title>
+                    <SubTitle>{movie.original_title === movie.title ? '' : `( ${movie.original_title} )`}</SubTitle>
+                    <Links>
+                        {movie.belongs_to_collection && (
+                            <CollectionLink to={`/collection/${movie.belongs_to_collection.id}`}>
+                                {movie.belongs_to_collection.name}
+                            </CollectionLink>
+                        )}
+                    </Links>
+                    <Classification genresLength={movie.genres && movie.genres.length}>
+                        <Year>
+                            {movie.release_date ? movie.release_date.slice(0, 4) : 'Not Updated'}
+                            <Divider>|</Divider>
+                        </Year>
+                        {movie.adult && (
+                            <Adult>
+                                Adult
+                                <Divider>|</Divider>
+                            </Adult>
+                        )}
+                        <Runtime>
+                            {movie.runtime} min
+                            <Divider>|</Divider>
+                        </Runtime>
+                        <Genres genres={movie.genres}></Genres>
+                        <Rank score={movie.vote_average} totalVotes={movie.vote_count} />
+                        <Divider>|</Divider>
+                        {movie.imdb_id && (
+                            <IMDbLink href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank">
+                                <IMDbLogo src="https://ia.media-imdb.com/images/M/MV5BMTk3ODA4Mjc0NF5BMl5BcG5nXkFtZTgwNDc1MzQ2OTE@._V1_.png" />
+                            </IMDbLink>
+                        )}
+                    </Classification>
+                    <Overview>{movie.overview}</Overview>
+                    <Actors id={movie.id} isMovie={true} />
+                    <Videos videos={movie.videos.results} />
+                </LeftBox>
+                <RightBox>
+                    <Poster
+                        posterUrl={
+                            movie.poster_path
+                                ? `https://image.tmdb.org/t/p/w400${movie.poster_path}`
+                                : require('../assets/no_poster.png')
+                        }
+                        isExisted={movie.poster_path && true}
+                    />
+                </RightBox>
+            </Container>
+        </>
+    );
 }
 
 MovieContent.propTypes = {
-	movie: PropTypes.shape({
-		title: PropTypes.string,
-		original_title: PropTypes.string,
-		belongs_to_collection: PropTypes.shape({
-			id: PropTypes.number.isRequired,
-			name: PropTypes.string.isRequired,
-		}),
-		release_date: PropTypes.string,
-		adult: PropTypes.bool,
-		runtime: PropTypes.number,
-		genres: PropTypes.array,
-		vote_average: PropTypes.number,
-		imdb_id: PropTypes.string,
-		overview: PropTypes.string,
-		id: PropTypes.number.isRequired,
-		videos: PropTypes.shape({
-			results: PropTypes.arrayOf(
-				PropTypes.shape({
-					id: PropTypes.string.isRequired,
-					key: PropTypes.string.isRequired,
-					name: PropTypes.string.isRequired,
-				})
-			),
-		}),
-		poster_path: PropTypes.string,
-	}).isRequired,
+    movie: PropTypes.shape({
+        title: PropTypes.string,
+        original_title: PropTypes.string,
+        belongs_to_collection: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }),
+        release_date: PropTypes.string,
+        adult: PropTypes.bool,
+        runtime: PropTypes.number,
+        genres: PropTypes.array,
+        vote_average: PropTypes.number,
+        imdb_id: PropTypes.string,
+        overview: PropTypes.string,
+        id: PropTypes.number.isRequired,
+        videos: PropTypes.shape({
+            results: PropTypes.arrayOf(
+                PropTypes.shape({
+                    id: PropTypes.string.isRequired,
+                    key: PropTypes.string.isRequired,
+                    name: PropTypes.string.isRequired,
+                })
+            ),
+        }),
+        poster_path: PropTypes.string,
+    }).isRequired,
 };
 
 export default withRouter(MovieContent);
