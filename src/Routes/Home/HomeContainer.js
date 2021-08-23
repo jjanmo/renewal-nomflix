7b4760cc4d1bf7b2dc22917c828b72d0ea0f5589
jjanmo <jjanmo07@hanmail.net>
2021-08-24 00:55:26 +0900

    fix: fix error about wrong accessed value when fetching video data

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -3,42 +3,42 @@ import HomePresenter from './HomePresenter';
 import { movieApi, tvApi } from 'api';
 
 export default class extends React.Component {
-    state = {
-        nowPlaying: [],
-        upcoming: [],
-        airingToday: [],
-        isLoading: true,
-        error: null,
-    };
+  state = {
+    nowPlaying: [],
+    upcoming: [],
+    airingToday: [],
+    isLoading: true,
+    error: null,
+  };
 
-    async componentDidMount() {
-        try {
-            const {
-                data: { results: nowPlaying },
-            } = await movieApi.nowPlaying();
-            const {
-                data: { results: upcoming },
-            } = await movieApi.upcoming();
-            const {
-                data: { results: airingToday },
-            } = await tvApi.airingToday();
-            this.setState({
-                nowPlaying,
-                upcoming,
-                airingToday,
-            });
-        } catch {
-            this.setState({
-                error: 'Can not find data...',
-            });
-        } finally {
-            this.setState({
-                isLoading: false,
-            });
-        }
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: upcoming },
+      } = await movieApi.upcoming();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        airingToday,
+      });
+    } catch {
+      this.setState({
+        error: 'Can not find data...',
+      });
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
     }
+  }
 
-    render() {
-        return <HomePresenter {...this.state} />;
-    }
+  render() {
+    return <HomePresenter {...this.state} />;
+  }
 }
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -9,99 +9,99 @@ import PropTypes from 'prop-types';
 const Container = styled.div``;
 
 function HomePresenter({ isLoading, nowPlaying, upcoming, airingToday }) {
-    return isLoading ? (
-        <>
-            <Helmet>
-                <title>Home | Nomflix</title>
-            </Helmet>
-            <Loader />
-        </>
-    ) : (
-        <Container>
-            <Helmet>
-                <title>Home | Nomflix</title>
-            </Helmet>
-            {nowPlaying && nowPlaying.length > 0 && (
-                <Section title="Now Playing Movies">
-                    {nowPlaying.map((movie) => (
-                        <Item
-                            key={movie.id}
-                            id={movie.id}
-                            title={movie.title}
-                            poster={movie.poster_path}
-                            rating={movie.vote_average}
-                            year={movie.release_date.slice(0, 4)}
-                            isMovie
-                        />
-                    ))}
-                </Section>
-            )}
-            {upcoming && upcoming.length > 0 && (
-                <Section title="Upcoming Movies">
-                    {upcoming.map((movie) => (
-                        <Item
-                            key={movie.id}
-                            id={movie.id}
-                            title={movie.title}
-                            poster={movie.poster_path}
-                            rating={movie.vote_average}
-                            year={movie.release_date.slice(0, 4)}
-                            isMovie
-                        />
-                    ))}
-                </Section>
-            )}
-            {airingToday && airingToday.length > 0 && (
-                <Section title="Today Airing TVs">
-                    {airingToday.map((tv) => (
-                        <Item
-                            key={tv.id}
-                            id={tv.id}
-                            title={tv.name}
-                            poster={tv.poster_path}
-                            rating={tv.vote_average}
-                            year={tv.first_air_date.slice(0, 4)}
-                            isMovie={false}
-                        />
-                    ))}
-                </Section>
-            )}
-        </Container>
-    );
+  return isLoading ? (
+    <>
+      <Helmet>
+        <title>Home | Nomflix</title>
+      </Helmet>
+      <Loader />
+    </>
+  ) : (
+    <Container>
+      <Helmet>
+        <title>Home | Nomflix</title>
+      </Helmet>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing Movies">
+          {nowPlaying.map((movie) => (
+            <Item
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              poster={movie.poster_path}
+              rating={movie.vote_average}
+              year={movie.release_date?.slice(0, 4)}
+              isMovie
+            />
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map((movie) => (
+            <Item
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              poster={movie.poster_path}
+              rating={movie.vote_average}
+              year={movie.release_date?.slice(0, 4)}
+              isMovie
+            />
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Today Airing TVs">
+          {airingToday.map((tv) => (
+            <Item
+              key={tv.id}
+              id={tv.id}
+              title={tv.name}
+              poster={tv.poster_path}
+              rating={tv.vote_average}
+              year={tv.first_air_date?.slice(0, 4)}
+              isMovie={false}
+            />
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 }
 
 HomePresenter.propTypes = {
-    isLoading: PropTypes.bool.isRequired,
-    nowPlaying: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            title: PropTypes.string,
-            poster_path: PropTypes.string,
-            vote_average: PropTypes.number,
-            release_date: PropTypes.string,
-            isMovie: PropTypes.bool,
-        })
-    ),
-    upcoming: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            title: PropTypes.string,
-            poster_path: PropTypes.string,
-            vote_average: PropTypes.number,
-            release_date: PropTypes.string,
-            isMovie: PropTypes.bool,
-        })
-    ),
-    airingToday: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            name: PropTypes.string,
-            poster_path: PropTypes.string,
-            vote_average: PropTypes.number,
-            first_air_date: PropTypes.string,
-            isMovie: PropTypes.bool,
-        })
-    ),
+  isLoading: PropTypes.bool.isRequired,
+  nowPlaying: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number,
+      release_date: PropTypes.string,
+      isMovie: PropTypes.bool,
+    })
+  ),
+  upcoming: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number,
+      release_date: PropTypes.string,
+      isMovie: PropTypes.bool,
+    })
+  ),
+  airingToday: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number,
+      first_air_date: PropTypes.string,
+      isMovie: PropTypes.bool,
+    })
+  ),
 };
 
 export default HomePresenter;
