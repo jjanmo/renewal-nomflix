0575589c713a164cd117140abd562480f6dd6af7
jjanmo <jjanmo07@hanmail.net>
2021-10-15 01:12:16 +0900

    refactor: refactor home page using hooks and delete original home container/presenter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Section from 'components/Section';
 import Loader from 'components/Loader';
 import Item from 'components/Item';
-import { Helmet } from 'react-helmet';
-import PropTypes from 'prop-types';
+import { movieApi, tvApi } from 'api';
+import HelmetTitle from '../components/HelmetTitle';
 
 const Container = styled.div``;
 
-function HomePresenter({ isLoading, nowPlaying, upcoming, airingToday }) {
+const Home = () => {
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [airingToday, setAiringToday] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const {
+        data: { results: _nowPlaying },
+      } = await movieApi.nowPlaying();
+
+      const {
+        data: { results: _upcoming },
+      } = await movieApi.upcoming();
+
+      const {
+        data: { results: _airingToday },
+      } = await tvApi.airingToday();
+
+      setNowPlaying(_nowPlaying);
+      setUpcoming(_upcoming);
+      setAiringToday(_airingToday);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) return <div>{JSON.stringify(error)}</div>;
+
   return isLoading ? (
     <>
-      <Helmet>
-        <title>Home | Nomflix</title>
-      </Helmet>
+      <HelmetTitle text="Home" />
       <Loader />
     </>
   ) : (
     <Container>
-      <Helmet>
-        <title>Home | Nomflix</title>
-      </Helmet>
+      <HelmetTitle text="Home" />
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing Movies">
           {nowPlaying.map(movie => (
@@ -68,40 +102,6 @@ function HomePresenter({ isLoading, nowPlaying, upcoming, airingToday }) {
       )}
     </Container>
   );
-}
-
-HomePresenter.propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-  nowPlaying: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      title: PropTypes.string,
-      poster_path: PropTypes.string,
-      vote_average: PropTypes.number,
-      release_date: PropTypes.string,
-      isMovie: PropTypes.bool,
-    })
-  ),
-  upcoming: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      title: PropTypes.string,
-      poster_path: PropTypes.string,
-      vote_average: PropTypes.number,
-      release_date: PropTypes.string,
-      isMovie: PropTypes.bool,
-    })
-  ),
-  airingToday: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      poster_path: PropTypes.string,
-      vote_average: PropTypes.number,
-      first_air_date: PropTypes.string,
-      isMovie: PropTypes.bool,
-    })
-  ),
 };
 
-export default HomePresenter;
+export default Home;
